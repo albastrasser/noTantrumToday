6e687a256c9b476b8daead53567e65e88254b4f4
Anna Strasser <albastrasser@gmail.com>
2021-05-02 15:41:56 -0400

    added a rules header

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,6 +6,10 @@ export default class Navbar extends Component {
     return (
       <nav>
         <img className={'title'} src="/annastitle.png" />
+        <h3>
+          Chat with your friends and make group decisions to try and get through
+          the morning without having a TANTRUM!
+        </h3>
       </nav>
     );
   }
